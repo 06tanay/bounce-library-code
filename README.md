fd5b51e4d2f7e6c5ac63f3a4ef1ad256aa108e4c
06tanay <75430984+06tanay@users.noreply.github.com>
2021-01-06 17:45:14 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# bounce-library-code
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,37 @@
+var fixedrect,movingrect;
+function setup() {
+
+  createCanvas(1200,800);
+  fixedrect=createSprite(400, 100, 50, 80);
+  fixedrect.shapeColor="green";
+fixedrect.debug=true;
+
+movingrect=createSprite(400,800,80,50);
+movingrect.shapeColor="green";
+movingrect.debug=true;
+  movingrect.velocityY=-5;
+  fixedrect.velocityY=5;
+}
+
+function draw() {
+  background(0);
+ 
+ // console.log(movingrect.x-fixedrect.x);
+  bounce(movingrect,fixedrect);
+
+  
+  drawSprites();
+}
+function bounce(object1,object2){
+  if(object1.x-object2.x<object2.width/2+object1.width/2&&
+    object2.x-object1.x<object2.width/2+object1.width/2){
+    object1.velocityX=object1.velocityX*(-1);
+    object2.velocityX=object2.velocityX*(-1);
+    
+  }
+ if(object1.y-object2.y<object2.width/2+object1.width/2&&
+object2.y-object1.y<object2.width/2+object1.width/2) {
+object1.velocityY=object1.velocityY*(-1);
+object2.velocityY=object2.velocityY*(-1);
+  }
+}
